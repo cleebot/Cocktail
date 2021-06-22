00c861d1422109f4a2df9494ae0971c8fcf42174
Seth Lee <sethlee@seths-air.attlocal.net>
2021-06-22 15:19:51 -0400

    js try catch

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const getCocktailList = async () => {
   try {
     const getCocktailURL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
     const response = await axios.get(getCocktailURL)
-    console.log(response)
+    // console.log(response.data.drinks[0])
+    const cocktailList = Object.keys(response.data.drinks[0])
+    console.log(cocktailList)
   }
   catch (error) {
   console.error(error)
