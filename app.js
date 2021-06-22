5ff8c310993f39a84079ced47194c055f6ee5940
Seth Lee <sethlee@seths-air.attlocal.net>
2021-06-22 14:55:21 -0400

    js axios get

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-let apiLink = ("www.thecocktaildb.com/api/json/v1/1/random.php")
+// `www.thecocktaildb.com/api/json/v1/1/random.php`
 
-let cocktailList = document.querySelector('.cocktail-list')
+const getCocktailList = async () => {
+  try {
+    const getCocktailURL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+    const response = await axios.get(getCocktailURL)
+    console.log(response)
+  }
+  catch (error) {
+  console.error(error)
+  }
+}
+
+getCocktailList()
